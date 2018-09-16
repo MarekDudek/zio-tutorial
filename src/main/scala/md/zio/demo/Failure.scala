0de0bf81e2ab78bb9e61e34349af865044334b90
Marek Dudek <marek.dudek@gmail.com>
2018-09-16 18:40:23 +0200

    failure

---
+++ src/main/scala/md/zio/demo/Failure.scala
@@ -0,0 +1,31 @@
+package md.zio.demo
+
+import java.io.{FileNotFoundException, IOException}
+
+import md.zio.demo.Purity.ImpureCode._
+import scalaz.zio._
+
+object Failure {
+
+  val failure: IO[String, Unit] = IO.fail("Oh noes!")
+
+  def readData(fileName: String): IO[IOException, String] = ???
+
+  val attempted: IO[Nothing, Either[IOException, String]] = readData("data.json").attempt
+
+  val absolved: IO[IOException, String] = IO.absolve(attempted)
+
+  val x: IO[Nothing, String] = readData("data.json").attempt.map {
+    case Left(_) => "42"
+    case Right(data) => data
+  }
+
+  val caughtAll: IO[Exception, Array[Byte]] = readFile("data.json").catchAll(_ => readFile("backup.json"))
+
+  val caughtSome: IO[Exception, Array[Byte]] = readFile("data.json").catchSome {
+    case _ => readFile("backup.json")
+  }
+
+
+
+}
