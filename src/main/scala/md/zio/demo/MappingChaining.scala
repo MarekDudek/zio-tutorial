c312d4efed42ab22fda811223af3970f867ec92d
Marek Dudek <marek.dudek@gmail.com>
2018-09-16 17:52:14 +0200

    chaining

--- src/main/scala/md/zio/demo/MappingChaining.scala
+++ src/main/scala/md/zio/demo/MappingChaining.scala
@@ -10,4 +10,19 @@ object MappingChaining {
 
     val x: IO[Exception, String] = IO.fail("No no!").leftMap(msg => new Exception(msg))
   }
+
+  object chaining {
+
+    val z: IO[Nothing, List[Int]] =
+      IO.point(List(1, 2, 3)).
+      flatMap{
+        list => IO.point(list.map(_ + 1))
+      }
+
+    val x: IO[Nothing, List[Int]] =
+      for {
+        list <- IO.point(List(1, 2, 3))
+        added <- IO.point(list.map(_ + 1))
+      } yield added
+  }
 }
